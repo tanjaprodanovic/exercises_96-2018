02e5f327aa9c541d191b20d97408aeb9695dcabb
Tanja Prodanovic <pttanja@gmail.com>
2020-11-09 21:04:48 +0100

    Commit kreiranje i model klase

---
+++ Exercises_96-2018/Exercises_96-2018/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercises_96_2018
+{
+    class ExerciseResult
+    {
+        public ExerciseResult(int id, string studentName, string studentIndex, int point)
+        {
+            Id = id;
+            StudentName = studentName;
+            StudentIndex = studentIndex;
+            Point = point;
+        }
+
+        public int Id { get; set; }
+        public string StudentName { get; set; }
+        public string StudentIndex { get; set; }
+        public int Point { get; set; }
+        public override string ToString()
+        {
+            return "Id:" + Id + "Student:" + StudentName + "Index:" + StudentIndex + "Points:" + Point;
+        }
+
+    }
+}
