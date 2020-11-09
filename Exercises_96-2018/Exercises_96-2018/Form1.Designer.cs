51a9b75e984aebdafbeeb512d0c92c451e58bd0a
Tanja Prodanovic <pttanja@gmail.com>
2020-11-09 21:19:06 +0100

    Commit nakon smestanja podataka iz tabele u listbbox

--- Exercises_96-2018/Exercises_96-2018/Form1.Designer.cs
+++ Exercises_96-2018/Exercises_96-2018/Form1.Designer.cs
@@ -28,13 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.listBoxExerciseResult = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // listBoxExerciseResult
+            // 
+            this.listBoxExerciseResult.FormattingEnabled = true;
+            this.listBoxExerciseResult.ItemHeight = 15;
+            this.listBoxExerciseResult.Location = new System.Drawing.Point(23, 11);
+            this.listBoxExerciseResult.Name = "listBoxExerciseResult";
+            this.listBoxExerciseResult.Size = new System.Drawing.Size(541, 274);
+            this.listBoxExerciseResult.TabIndex = 0;
+            this.listBoxExerciseResult.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBoxExerciseResult);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox listBoxExerciseResult;
     }
 }
 
